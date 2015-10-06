b61de8ad9984b8b56e0cc1392103d6e999a4a4ae
Leander (Macbook) <me@leander.ca>
2015-10-06 16:40:11 -0700

    Stub and editorconfig

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
+var request = require('request');
+var qs = require('querystring');
 
-function Admin (couchUrl, adminUser, adminPass) {
+function Admin (opts) {
 
-	
-	
+  opts = opts || {};
+
+  this.url = opts.url || 'http://localhost:5984';
+  this.user = opts.user || 'admin';
+  this.pass = opts.pass || 'mysecretpassword';
+  this.db = opts.db || '_users';
+
+}
+
+Admin.prototype._request = function (method, db, data) {
+
+}
+
+Admin.prototype.createUser = function (user, pass, cb) {
+  cb(null);
+}
+Admin.prototype.changePassword = function (user, pass, cb) {
+  cb(null);
+}
+Admin.prototype.createDatabase = function (db, cb) {
+  cb(null);
+}
+Admin.prototype.grantMembership = function (user, db, cb) {
+  cb(null);
+}
+Admin.prototype.revokeMembership = function (user, db, cb) {
+  cb(null);
+}
+Admin.prototype.grantAdmin = function (user, db, cb) {
+  cb(null);
+}
+Admin.prototype.revokeAdmin = function (user, db, cb) {
+  cb(null);
+}
+Admin.prototype.removeUser = function (user, cb) {
+  cb(null);
+}
+Admin.prototype.removeDatabase = function (db, cb) {
+  cb(null);
 }
 
 module.exports = Admin;
